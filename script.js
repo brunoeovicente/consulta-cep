a7b8592b35b43cd7204e7cbe28ffef7b1e13fbb7
unknown <brunoeovicente@gmail.com>
2021-09-24 20:19:32 -0300

    rascunho HTML e CSS

---
+++ script.js
@@ -0,0 +1,7 @@
+const cep = document.getElementById("cep")
+const logradouro = document.getElementById("logradouro")
+const complemento = document.getElementById("complemento")
+const bairro = document.getElementById("bairro")
+const localidade = document.getElementById("localidade")
+const uf = document.getElementById("uf")
+
